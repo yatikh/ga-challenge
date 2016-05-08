c3ab28d1c82979f787e0ca4213aa36ffd90453bb
Yaroslav Tikhomirov <yartikh@gmail.com>
2016-05-08 10:47:58 +0500

    fixed country model namespace

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
